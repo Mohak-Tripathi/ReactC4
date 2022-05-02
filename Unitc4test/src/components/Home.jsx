45b2a13e8347f59a7cb5ba998c0ec339b032f034
Mohak-Tripathi <mohaktripathi029@gmail.com>
2022-05-02 17:16:06 +0530

    final commit

--- Unitc4test/src/components/Home.jsx
+++
@@ -1,7 +0,0 @@
-export const Home = () => {
-    return (
-      <div className="homepage">
-        Welcome to Laptop service center. Please login to continue this service
-      </div>
-    );
-  };
--- Unitc4test/src/components/ProtextedRoute.jsx
+++
@@ -1,4 +0,0 @@
-export const ProtectedRoute = ({ children }) => {
-  
-    return children  
-  };
